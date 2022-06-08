b81c8a7ff2345bea175813d6fe9ffea6778317ec
Alexandre Gattiker <algattik@microsoft.com>
2022-06-08 20:06:58 +0200

    .

--- system-tests/src/test/java/org/eclipse/dataspaceconnector/registration/client/RegistrationApiClientTest.java
+++ system-tests/src/test/java/org/eclipse/dataspaceconnector/registration/client/RegistrationApiClientTest.java
@@ -6,15 +6,11 @@ import org.eclipse.dataspaceconnector.registration.client.models.Participant;
 import org.junit.jupiter.api.Test;
 import picocli.CommandLine;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SuppressWarnings("InstantiationOfUtilityClass")
@@ -30,9 +26,6 @@ public class RegistrationApiClientTest {
         CheckSum app = new CheckSum();
         CommandLine cmd = new CommandLine(app);
 
-        StringWriter sw = new StringWriter();
-        cmd.setOut(new PrintWriter(sw));
-
         var strings = new ArrayList<String>();
         strings.add("register");
         strings.addAll(List.of("-s", API_URL));
@@ -41,14 +34,8 @@ public class RegistrationApiClientTest {
         Objects.requireNonNull(participant.getSupportedProtocols()).forEach(p -> strings.addAll(List.of("-p", p)));
         int exitCode = cmd.execute(strings.toArray(String[]::new));
         assertEquals(0, exitCode);
-        assertEquals("", sw.toString());
 
-// white box testing
-
-        await().atMost(Duration.ofSeconds(10))
-                .untilAsserted(() ->
-                        assertThat(api.listParticipants())
-                                .containsExactly(participant));
-
+        assertThat(api.listParticipants())
+                .containsExactly(participant);
     }
 }
