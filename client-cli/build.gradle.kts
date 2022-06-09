611b482370b990b055e9784635cc01af505c3122
Alexandre Gattiker <algattik@microsoft.com>
2022-06-09 12:06:11 +0200

    .

---
+++ client-cli/build.gradle.kts
@@ -0,0 +1,54 @@
+/*
+ *  Copyright (c) 2020, 2021 Microsoft Corporation
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Microsoft Corporation - initial API and implementation
+ *
+ */
+
+plugins {
+    `java-library`
+    id("application")
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+    `maven-publish`
+}
+
+val edcVersion: String by project
+val edcGroup: String by project
+val jacksonVersion: String by project
+
+dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
+
+    api("info.picocli:picocli:4.6.3")
+    api(project(":rest-client"))
+    annotationProcessor("info.picocli:picocli-codegen:4.6.3")
+}
+
+tasks.withType<JavaCompile> {
+    options.compilerArgs.add("-Aproject=${project.group}/${project.name}")
+}
+
+application {
+    mainClass.set("org.eclipse.dataspaceconnector.registration.cli.RegistrationServiceCli")
+}
+
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    mergeServiceFiles()
+    archiveFileName.set("registration-service-cli.jar")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("registration-service-cli") {
+            artifactId = "registration-service-cli"
+            from(components["java"])
+        }
+    }
+}
---
+++ client-cli/src/main/java/org/eclipse/dataspaceconnector/registration/cli/RegistrationServiceCli.java
@@ -0,0 +1,20 @@
+package org.eclipse.dataspaceconnector.registration.cli;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(name = "registration-service-cli", mixinStandardHelpOptions = true,
+        description = "Client utility for MVD registration service.",
+        subcommands = {
+                ParticipantsCommand.class
+        })
+@SuppressWarnings("InstantiationOfUtilityClass")
+public class RegistrationServiceCli {
+    @CommandLine.Option(names = "-s", required = true, description = "Registration service URL")
+    String service;
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new RegistrationServiceCli()).execute(args);
+        System.exit(exitCode);
+    }
+}
