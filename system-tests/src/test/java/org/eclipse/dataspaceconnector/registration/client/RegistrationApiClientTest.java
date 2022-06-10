d44ba7a3ffa660747be9a52073ccc6b4c4add946
Alexandre Gattiker <algattik@microsoft.com>
2022-06-10 07:47:39 +0200

    .

--- system-tests/src/test/java/org/eclipse/dataspaceconnector/registration/client/RegistrationApiClientTest.java
+++ system-tests/src/test/java/org/eclipse/dataspaceconnector/registration/client/RegistrationApiClientTest.java
@@ -1,36 +1,36 @@
 package org.eclipse.dataspaceconnector.registration.client;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.eclipse.dataspaceconnector.registration.cli.RegistrationServiceCli;
-import org.eclipse.dataspaceconnector.registration.client.api.RegistryApi;
 import org.eclipse.dataspaceconnector.registration.client.models.Participant;
 import org.junit.jupiter.api.Test;
 import picocli.CommandLine;
 
-import java.util.ArrayList;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.List;
-import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.eclipse.dataspaceconnector.registration.client.IntegrationTestUtils.createParticipant;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // @IntegrationTest
 public class RegistrationApiClientTest {
-    ApiClient apiClient = ApiClientFactory.createApiClient(API_URL);
-    RegistryApi api = new RegistryApi(apiClient);
-
     @Test
-    void listParticipants() {
+    void listParticipants() throws Exception {
         RegistrationServiceCli app = new RegistrationServiceCli();
         CommandLine cmd = new CommandLine(app);
 
-        var strings = new ArrayList<String>();
-        strings.add("participants");
-        strings.add("list");
-        int exitCode = cmd.execute(strings.toArray(String[]::new));
+        StringWriter sw = new StringWriter();
+        cmd.setOut(new PrintWriter(sw));
+
+        int exitCode = cmd.execute("participants", "list");
         assertEquals(0, exitCode);
 
-        assertThat(api.listParticipants())
-                .containsExactly(participant);
+        String s = sw.toString();
+        var participants = new ObjectMapper().readValue(s, new TypeReference<List<Participant>>() {
+        });
+        assertThat(participants).hasSize(3);
+        assertThat(participants).extracting(Participant::getName).contains("consumer-eu");
     }
 }
