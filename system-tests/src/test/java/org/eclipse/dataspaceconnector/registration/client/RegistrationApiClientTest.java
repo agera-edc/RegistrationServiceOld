20b163ebccce9d0e26575bc8e119cb0fdfbde6ce
Alexandre Gattiker <algattik@microsoft.com>
2022-06-10 07:20:53 +0200

    y

--- system-tests/src/test/java/org/eclipse/dataspaceconnector/registration/client/RegistrationApiClientTest.java
+++ system-tests/src/test/java/org/eclipse/dataspaceconnector/registration/client/RegistrationApiClientTest.java
@@ -1,29 +1,36 @@
 package org.eclipse.dataspaceconnector.registration.client;
 
+import org.eclipse.dataspaceconnector.registration.cli.RegistrationServiceCli;
 import org.eclipse.dataspaceconnector.registration.client.api.RegistryApi;
 import org.eclipse.dataspaceconnector.registration.client.models.Participant;
 import org.junit.jupiter.api.Test;
+import picocli.CommandLine;
 
-import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.awaitility.Awaitility.await;
+import static org.eclipse.dataspaceconnector.registration.client.IntegrationTestUtils.createParticipant;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@IntegrationTest
+// @IntegrationTest
 public class RegistrationApiClientTest {
-
-    static final String API_URL = "http://localhost:8181/api";
-
     ApiClient apiClient = ApiClientFactory.createApiClient(API_URL);
     RegistryApi api = new RegistryApi(apiClient);
 
     @Test
     void listParticipants() {
-        await().atMost(Duration.ofSeconds(30))
-                .untilAsserted(() ->
-                        assertThat(api.listParticipants())
-                        .extracting(Participant::getName)
-                        .containsExactlyInAnyOrder("consumer-eu", "consumer-us", "provider"));
+        RegistrationServiceCli app = new RegistrationServiceCli();
+        CommandLine cmd = new CommandLine(app);
 
+        var strings = new ArrayList<String>();
+        strings.add("participants");
+        strings.add("list");
+        int exitCode = cmd.execute(strings.toArray(String[]::new));
+        assertEquals(0, exitCode);
+
+        assertThat(api.listParticipants())
+                .containsExactly(participant);
     }
 }
