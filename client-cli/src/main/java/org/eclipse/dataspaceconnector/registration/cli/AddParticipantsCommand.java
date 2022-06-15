f601277ed05c92141d8ffafaf8a61d44c1cf846d
Peeyush Chandel <555114+cpeeyush@users.noreply.github.com>
2022-06-15 17:25:11 +0200

    Add participant command

--- client-cli/src/main/java/org/eclipse/dataspaceconnector/registration/cli/AddParticipantsCommand.java
+++ client-cli/src/main/java/org/eclipse/dataspaceconnector/registration/cli/AddParticipantsCommand.java
@@ -15,14 +15,7 @@
 package org.eclipse.dataspaceconnector.registration.cli;
 
 import picocli.CommandLine;
-import picocli.CommandLine.Command;
 
-@Command(name = "participants", mixinStandardHelpOptions = true,
-        description = "Manage dataspace participants.",
-        subcommands = {
-                ListParticipantsCommand.class
-        })
-class ParticipantsCommand {
-    @CommandLine.ParentCommand
-    RegistrationServiceCli parent;
+@CommandLine.Command(name = "add")
+public class AddParticipantsCommand {
 }
